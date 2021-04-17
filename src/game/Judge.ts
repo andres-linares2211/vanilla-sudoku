64719b9a4672dc991be508087c80d8da878f35e1
andres-linares2211 <andres.linares@omni.pro>
2021-04-17 18:13:39 -0500

    refactor: shorter code in Judge

--- src/game/Judge.ts
+++ src/game/Judge.ts
@@ -1,3 +1,5 @@
+import { QUADRANT_INDEXES } from './constants.js';
+
 export class Judge {
   isValidGame(values: number[]): boolean {
     if (values.length !== 9 * 9) return false;
@@ -45,15 +47,15 @@ export class Judge {
   }
 
   private validColumns(values: number[]): boolean {
-    const column1 = values.filter((value, index) => index % 9 === 0);
-    const column2 = values.filter((value, index) => index % 9 === 1);
-    const column3 = values.filter((value, index) => index % 9 === 2);
-    const column4 = values.filter((value, index) => index % 9 === 3);
-    const column5 = values.filter((value, index) => index % 9 === 4);
-    const column6 = values.filter((value, index) => index % 9 === 5);
-    const column7 = values.filter((value, index) => index % 9 === 6);
-    const column8 = values.filter((value, index) => index % 9 === 7);
-    const column9 = values.filter((value, index) => index % 9 === 8);
+    const column1 = values.filter((_, index) => index % 9 === 0);
+    const column2 = values.filter((_, index) => index % 9 === 1);
+    const column3 = values.filter((_, index) => index % 9 === 2);
+    const column4 = values.filter((_, index) => index % 9 === 3);
+    const column5 = values.filter((_, index) => index % 9 === 4);
+    const column6 = values.filter((_, index) => index % 9 === 5);
+    const column7 = values.filter((_, index) => index % 9 === 6);
+    const column8 = values.filter((_, index) => index % 9 === 7);
+    const column9 = values.filter((_, index) => index % 9 === 8);
 
     const validColumn1 = this.allAreDifferent(column1);
     const validColumn2 = this.allAreDifferent(column2);
@@ -79,105 +81,17 @@ export class Judge {
   }
 
   private validQuadrants(values: number[]) {
-    const quadrant1 = [
-      values[0],
-      values[1],
-      values[2],
-      values[9],
-      values[10],
-      values[11],
-      values[18],
-      values[19],
-      values[20],
-    ];
-    const quadrant2 = [
-      values[3],
-      values[4],
-      values[5],
-      values[12],
-      values[13],
-      values[14],
-      values[21],
-      values[22],
-      values[23],
-    ];
-    const quadrant3 = [
-      values[6],
-      values[7],
-      values[8],
-      values[15],
-      values[16],
-      values[17],
-      values[24],
-      values[25],
-      values[26],
-    ];
-    const quadrant4 = [
-      values[27],
-      values[28],
-      values[29],
-      values[36],
-      values[37],
-      values[38],
-      values[45],
-      values[46],
-      values[47],
-    ];
-    const quadrant5 = [
-      values[30],
-      values[31],
-      values[32],
-      values[39],
-      values[40],
-      values[41],
-      values[48],
-      values[49],
-      values[50],
-    ];
-    const quadrant6 = [
-      values[33],
-      values[34],
-      values[35],
-      values[42],
-      values[43],
-      values[44],
-      values[51],
-      values[52],
-      values[53],
-    ];
-    const quadrant7 = [
-      values[54],
-      values[55],
-      values[56],
-      values[63],
-      values[64],
-      values[65],
-      values[72],
-      values[73],
-      values[74],
-    ];
-    const quadrant8 = [
-      values[57],
-      values[58],
-      values[59],
-      values[66],
-      values[67],
-      values[68],
-      values[75],
-      values[76],
-      values[77],
-    ];
-    const quadrant9 = [
-      values[60],
-      values[61],
-      values[62],
-      values[69],
-      values[70],
-      values[71],
-      values[78],
-      values[79],
-      values[80],
-    ];
+    const [
+      quadrant1,
+      quadrant2,
+      quadrant3,
+      quadrant4,
+      quadrant5,
+      quadrant6,
+      quadrant7,
+      quadrant8,
+      quadrant9,
+    ] = QUADRANT_INDEXES.map((quadrant) => quadrant.map((index) => values[index]));
 
     const validQuadrant1 = this.allAreDifferent(quadrant1);
     const validQuadrant2 = this.allAreDifferent(quadrant2);
