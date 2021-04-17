2c7feb77a645cfd5cdcfad7b8fdeb9a27ede54d8
andres-linares2211 <andres.linares@omni.pro>
2021-04-17 16:14:54 -0500

    feat: improve generation with row validation

--- src/game/BoardGenerator.ts
+++ src/game/BoardGenerator.ts
@@ -8,27 +8,37 @@ export class BoardGenerator {
     let invalidGame = true;
     let count = 0;
 
+    const initialTime = performance.now();
+
     while (invalidGame) {
       this.numbers = [];
       const countsArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      let currentRow = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
       for (let i = 0; i < 9 * 9; i++) {
+        if (i % 9 === 0) currentRow = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
         let randomNumber = Math.floor(Math.random() * 9) + 1;
-        while (countsArray[randomNumber - 1] >= 9) {
+        while (countsArray[randomNumber - 1] >= 9 || currentRow[randomNumber - 1] === 1) {
           randomNumber = Math.floor(Math.random() * 9) + 1;
         }
 
         countsArray[randomNumber - 1] += 1;
+        currentRow[randomNumber - 1] += 1;
+
         this.numbers.push(randomNumber);
       }
 
       invalidGame = !this.judge.isValidGame(this.numbers);
       count++;
 
-      if (count > 999) break;
+      if (count > 99999) break;
     }
 
-    console.log({ invalidGame });
+    const finalTime = performance.now();
+    console.log(`Board generated in ${(finalTime / 1000).toFixed(2)}s`);
+
+    console.log({ invalidGame, count });
 
     return this.numbers;
   }
--- src/game/Judge.ts
+++ src/game/Judge.ts
@@ -3,8 +3,43 @@ export class Judge {
     if (values.length !== 9 * 9) return false;
     if (!values.every((value) => value >= 1 && value <= 9)) return false;
     if (!this.validAmountOfValues(values)) return false;
+    if (!this.validRows(values)) return false;
 
     return true;
+  }
+
+  private validRows(values: number[]): boolean {
+    const row1 = values.slice(0, 9);
+    const row2 = values.slice(9, 18);
+    const row3 = values.slice(18, 27);
+    const row4 = values.slice(27, 36);
+    const row5 = values.slice(36, 45);
+    const row6 = values.slice(45, 54);
+    const row7 = values.slice(54, 63);
+    const row8 = values.slice(63, 72);
+    const row9 = values.slice(72, 81);
+
+    const validRow1 = this.allAreDifferent(row1);
+    const validRow2 = this.allAreDifferent(row2);
+    const validRow3 = this.allAreDifferent(row3);
+    const validRow4 = this.allAreDifferent(row4);
+    const validRow5 = this.allAreDifferent(row5);
+    const validRow6 = this.allAreDifferent(row6);
+    const validRow7 = this.allAreDifferent(row7);
+    const validRow8 = this.allAreDifferent(row8);
+    const validRow9 = this.allAreDifferent(row9);
+
+    return (
+      validRow1 &&
+      validRow2 &&
+      validRow3 &&
+      validRow4 &&
+      validRow5 &&
+      validRow6 &&
+      validRow7 &&
+      validRow8 &&
+      validRow9
+    );
   }
 
   private validAmountOfValues(values: number[]) {
