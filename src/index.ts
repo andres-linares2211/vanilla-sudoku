aa2998cdb28008ff23133eeadd99521097e6591d
andres-linares2211 <andres.linares@omni.pro>
2021-04-17 19:55:39 -0500

    feat: add highlight

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Board } from './game/Board.js';
+import { QUADRANT_INDEXES } from './game/constants.js';
 
 const app = document.getElementById('root');
+const inputs: HTMLInputElement[] = [];
 let game: Board;
 
 initialize();
@@ -21,14 +23,43 @@ function paint() {
     input.setAttribute('type', 'number');
     if (value) {
       input.value = value.toString();
-      input.disabled = true;
+      input.readOnly = true;
     }
+
+    const hightlight = (event: FocusEvent | MouseEvent) => {
+      const quadrantIndexes = QUADRANT_INDEXES.find((quadrant) => quadrant.includes(i));
+      quadrantIndexes?.forEach((index) => inputs[index].classList.add('highlight', 'soft'));
+
+      const firstIndexInColumn = i % 9;
+      for (let i = 0; i < 9; i++) {
+        inputs[firstIndexInColumn + i * 9].classList.add('highlight');
+        inputs[firstIndexInColumn + i * 9].classList.remove('soft');
+      }
+
+      const firstIndexInRow = Math.floor(i / 9) * 9;
+      for (let i = 0; i < 9; i++) {
+        inputs[firstIndexInRow + i].classList.add('highlight');
+        inputs[firstIndexInRow + i].classList.remove('soft');
+      }
+    };
+
+    const unHightlight = (event: FocusEvent | MouseEvent) => {
+      const input = event.target as HTMLInputElement;
+      input.classList.remove('highlight');
+      inputs.forEach((input) => input.classList.remove('highlight', 'soft'));
+    };
+
+    input.addEventListener('focus', hightlight);
+    input.addEventListener('mouseenter', hightlight);
+    input.addEventListener('blur', unHightlight);
+    input.addEventListener('mouseleave', unHightlight);
 
     if (i % 27 < 9) input.classList.add('border-top');
     if (i + 1 > 9 * 9 - 9) input.classList.add('border-bottom');
     if ((i + 1) % 3 === 0) input.classList.add('border-right');
     if (i % 9 === 0) input.classList.add('border-left');
 
+    inputs.push(input);
     app?.appendChild(input);
   }
 }
