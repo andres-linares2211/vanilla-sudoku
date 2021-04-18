a2eabbb60c5b7b6e278ead51f276565d8158ce33
andres-linares2211 <andres.linares@omni.pro>
2021-04-18 10:52:11 -0500

    feat: add error checker

--- src/game/Validator.ts
+++ src/game/Validator.ts
@@ -3,7 +3,8 @@ import { Finder } from './Finder.js';
 export class Validator {
   private finder = new Finder();
 
-  validate(value: number, index: number, values: number[]): boolean {
+  validate(value: number, index: number, values: (number | null)[]): boolean {
+    console.log('hello', { value, index, values });
     const possibleValues = this.finder.getPossibleValues(index, values);
 
     return possibleValues.includes(value);
