8a390c0444bc5f089ca0f592a93ce2906d5c8d7a
andres-linares2211 <andres.linares@omni.pro>
2021-04-18 11:46:44 -0500

    feat: split initial values with current values

--- src/game/Board.ts
+++ src/game/Board.ts
@@ -2,12 +2,22 @@ import { BoardGenerator } from './BoardGenerator.js';
 
 export class Board {
   private numbers: (number | null)[] = [];
+  private currentNumbers: (number | null)[] = [];
 
   initialize() {
     this.numbers = new BoardGenerator().generateBoard();
+    this.currentNumbers = [...this.numbers];
   }
 
-  get values() {
+  setValue(index: number, value: number | null) {
+    this.currentNumbers[index] = value;
+  }
+
+  get initialValues() {
     return this.numbers;
+  }
+
+  get currentValues() {
+    return this.currentNumbers;
   }
 }
