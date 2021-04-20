31c171605bfc6d24eb8818c407136187535b3aaf
andres-linares2211 <andres.linares@omni.pro>
2021-04-20 17:31:01 -0500

    refactor: use shorter code in translator

--- src/ui/Translator.ts
+++ src/ui/Translator.ts
@@ -4,7 +4,7 @@ getTranslationFile(language).then((file) => translateWithFile(file));
 type translationFile = { [key: string]: string };
 
 function translateWithFile(file: translationFile) {
-  document.querySelectorAll('[data-translate]').forEach(async (element) => {
+  document.querySelectorAll('[data-translate]').forEach((element) => {
     const key = element.getAttribute('data-translate');
     if (!key) return;
 
@@ -19,10 +19,9 @@ async function getTranslationFile(language: string): Promise<translationFile> {
   let file = null;
 
   try {
-    file = await import('../../i18n/en.json');
     file = await import(`../../i18n/${language.split('-')[0]}.json`);
   } catch (e) {
-    console.warn(`We couldn't file a translation file for ${language}`);
+    file = await import('../../i18n/en.json');
   }
 
   return file;
